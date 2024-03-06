afeca6c5b435cebdf337dbd6e9b39b1917282514
Teklu Mezgebo <mezge002@umn.edu>
2024-03-06 10:22:02 -0600

    async action created with the use of thunk middleware

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,11 @@
-// write your CatList component here
+import React from "react";
+
+function CatList({ catPics }) {
+    return (
+        <ul>
+            {catPics.length > 0 ? catPics.map((catPic) => <img alt="Cute cat" key={catPic.id} src={catPics.url}/>) : null}
+        </ul>
+    )
+}
+
+export default CatList
--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,23 @@
 import React from "react";
+import CatList from "./CatList";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCats } from "./catsSlice";
 
 function Cats() {
+
+  const catPics = useSelector((state) => state.entities)
+  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchCats())
+  }, [])
+
   return (
     <div>
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      <CatList catPics={catPics}/>
     </div>
   );
 }
